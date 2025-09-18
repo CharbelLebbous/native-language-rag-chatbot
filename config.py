4eb1e9d309c8e66da6c6db1b310ce25809ca4575
Charbel LEBBOUS <charbellebbousf@gmail.com>
2025-09-18 18:40:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ import os
 # ✅ Define your Cohere API key here.
 # This line tries to read the `COHERE_API_KEY` from your system's environment variables.
 # If it's not set, it falls back to the hardcoded default `"YOUR_API_KEY"` as a placeholder.
-COHERE_API_KEY = os.getenv("COHERE_API_KEY", "t29LSJzyWK9a2NrZWBwQ4exWf1e71STklQLgx6OJ")
+COHERE_API_KEY = os.getenv("COHERE_API_KEY", "YOUR_API_KEY")
